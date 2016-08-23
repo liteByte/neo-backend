bf5a4801d84df4dde2a3c062e6ef731b4b45e8b6
Agustín Argüello <agus.ap96@gmail.com>
2016-08-23 11:59:46 -0300

    Add http router

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: go-getting-started
+web: neo-backend
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+    "log"
+    "net/http"
+    "os"
     "fmt"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-    fmt.Print("Holis")
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
+
+    router := gin.New()
+    router.Use(gin.Logger())
+    // router.LoadHTMLGlob("templates/*.tmpl.html")
+    // router.Static("/static", "static")
+
+    // router.GET("/", func(c *gin.Context) {
+    //     c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    // })
+
+    fmt.Print("Test net/http library const: " , http.StatusOK)
+
+    router.Run(":" + port)
 }
