f5a676e8bc8d51921919919144fef040a3edb34c
Agustín Argüello <agus.ap96@gmail.com>
2016-08-25 11:24:26 -0300

    Added getApodJSON() func

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
     "net/http"
     "os"
     "github.com/gin-gonic/gin"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
 )
 
 func main() {
+
+    fmt.Print("Hi!")
+
     port := os.Getenv("PORT")
+    // port = "5000"
 
     if port == "" {
         log.Fatal("$PORT must be set")
@@ -18,12 +25,42 @@ func main() {
     router.Use(gin.Logger())
 
     router.GET("/neo/feed", func(c *gin.Context) {
-        c.String(http.StatusOK, "This wiil return Neo feed")
+        ApodJSON := getApodJSON()
+        c.JSON(http.StatusOK, ApodJSON)
     })
 
     router.GET("/planetary/apod", func(c *gin.Context) {
-        c.String(http.StatusOK, "This wiil return the APOD")
+        c.String(http.StatusOK, "This will return the APOD")
     })
 
+    // ApodJSON := getApodJSON()
+    // fmt.Printf("JSON body: %v \n JSON type: %t \n", ApodJSON, ApodJSON)
+
     router.Run(":" + port)
+}
+
+type Apod struct {
+    Url string
+}
+
+func getApodJSON() Apod{
+
+    ApodJSON := Apod{}
+
+    resp, err := http.Get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+    if (err != nil){
+        return ApodJSON
+    }
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+
+    err = json.Unmarshal(body, &ApodJSON)
+
+    if (err != nil){
+        return ApodJSON
+    }
+
+    return ApodJSON
 }
